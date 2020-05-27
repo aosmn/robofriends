575be6695d1fa550c02b898651cb8996dc6ef943
Alaa Osman <alaa.osmana@gmail.com>
2020-05-27 12:00:45 +0200

    Updates

---
+++ precache-manifest.f65f3fb094f240c703aac466591e4c9e.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "74cdcfbed335f1c42b27336ae245160f",
+    "url": "/robofriends/index.html"
+  },
+  {
+    "revision": "5e966f12b4aefc0bf19f",
+    "url": "/robofriends/static/css/2.3c03f26f.chunk.css"
+  },
+  {
+    "revision": "a94faa507163eaf0ceed",
+    "url": "/robofriends/static/css/main.df47e1ba.chunk.css"
+  },
+  {
+    "revision": "5e966f12b4aefc0bf19f",
+    "url": "/robofriends/static/js/2.4fb3063d.chunk.js"
+  },
+  {
+    "revision": "bb8fb1c4ca51bfe3d3f76c8caf6f2c92",
+    "url": "/robofriends/static/js/2.4fb3063d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a94faa507163eaf0ceed",
+    "url": "/robofriends/static/js/main.3b8d719b.chunk.js"
+  },
+  {
+    "revision": "99557a09ab68caec6407",
+    "url": "/robofriends/static/js/runtime-main.229ccf3b.js"
+  }
+]);
